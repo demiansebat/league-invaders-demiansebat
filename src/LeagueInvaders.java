3bdb4c75bb19af525d0d861654b54cf4fb26ad2d
league <league@imac-109.attlocal.net>
2021-12-10 16:53:50 -0800

    began leagueinvaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,25 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	JFrame frame;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	GamePanel  gamething;
 
+	public static void main(String[] args) {
+	LeagueInvaders league=new LeagueInvaders();
+	league.setup();
+	}
+
+	LeagueInvaders() {
+		frame = new JFrame();
+		gamething= new GamePanel();
+	}
+
+	void setup() {
+		frame.add(gamething);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
 }
